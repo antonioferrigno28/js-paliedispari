ea218ac8d9645c3ca8fadcd4e98c56c8f7e5447c
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-18 13:32:53 +0200

    Super simplified structure without function

---
+++ es1-palindromo/JS/script.js
@@ -0,0 +1,16 @@
+/* ESERCIZIO 1: PAROLA PALINDROMA
+
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// chiedere una parola all'utente
+const userWord = prompt("Inserisci una parola.");
+console.log(userWord);
+
+//se la parola è palindroma
+//Display messaggio di parola palindroma
+//else
+//Display di messaggio di parola NON palindroma
