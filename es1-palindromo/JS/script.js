63c0369a61e34ed046bfb48532cdbfffb1a279c4
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-18 15:15:21 +0200

    Es 1 DONE!, ES2 START

--- es1-palindromo/JS/script.js
+++ es1-palindromo/JS/script.js
@@ -7,10 +7,38 @@ Creare una funzione per capire se la parola inserita è palindroma */
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //funzioni
 
-//funzione di verifica parola palindroma
+//funzione di verifica parola
+function reversedComparison() {
+  //dichiarazione in una variabile della lunghezza della parola inserita dall'utente
+  const userWordLength = userWord.length;
+  //PER i che parte da 0 fino ad arrivare alla metà della lunghezza della parola incrementa i
+  for (let i = 0; i < userWordLength / 2; i++) {
+    //SE il carattere attuale della parola inserita dall'utente NON è uguale al carattere attuale nella posizione opposta della parola inserita dall'utente
+    if (userWord[i] !== userWord[userWordLength - 1 - i]) {
+      //la parola non è palindroma
+      return false;
+    }
+  }
+  //ALTRIMENTI
+
+  //la parola è palindroma
+  return true;
+}
 
 //funzione di output messaggio all'utente (se palindroma o no)
+
+function isWordPalindrome(userWord) {
+  const isPalindrome = reversedComparison(userWord);
+  if (isPalindrome) {
+    alert("La parola che hai inserito è palindroma.");
+  } else {
+    alert("La parola che hai inserito non è palindroma.");
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // chiedere una parola all'utente
 const userWord = prompt("Inserisci una parola.");
 //chiamare la funzione sulla parola dell'utente
+isWordPalindrome(userWord);
---
+++ es2-pari-dispari/JS/script.js
@@ -0,0 +1,25 @@
+/* ESERCIZIO 2: PARI E DISPARI
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//FUNZIONI
+
+// Funzione per generare un numero tra 1 e 5
+
+// Funzione per verificare se un numero è pari
+
+// Funzione per mostrare il risultato
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// chiedere all'utente "pari" o "dispari"
+// chiedere un numero da 1 a 5 all'utente
+// generare un numero per il computer
+// sommare i due numeri
+// determinare se la somma è pari o dispari
+// chiamare la funzione per mostrare il risultato
